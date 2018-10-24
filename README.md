aa65564e89f9fc4e57e8578619cb77ed4a2e2844
arashi walker, Devin Sweeting and Miles Bradt <miles@>
2018-10-24 13:38:11 -0700

    run user input to determine if first letter of a word is a vowel

---
+++ README.md
@@ -0,0 +1,64 @@
+# Pig Latin
+
+#### Pig Latin Scrambler, Oct 24th 2018
+
+#### By Devin Sweeting, Arashi Walker & Miles
+
+## Description
+
+_{This is a detailed description of your application. Its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }_
+
+
+
+##Specs
+. This program will ignore any nonnumerical values.
+
+2. This program will recognize a vowel with a true/false statement.
+    Input example: "a"
+    Output Example: "true"
+
+3. This program will recognize a vowel in a word with a true/false statement
+    Input Example: "cat"
+    Output Example: "true"
+
+4. This program will add "ay" to single-letter words beginning with a vowel
+    Input Example: "I"
+    Output Example: "Iay"
+
+5.  For words beginning with one or more consonants, move all of the first consecutive consonants to the end, and add "ay".
+    Input Example: Start
+    Output Example: artstay
+
+6.  If the first consonants include "qu", move the "u" along with the "q". Don't forget about words like "squeal" where "qu" doesn't come first!
+    Input Example: squirt
+    Output Example: irtsquay
+
+7.  For words beginning with "y", treat "y" as a consonant.
+    Input Example: your
+    Output Example: ouyay
+
+## Setup/Installation Requirements
+
+    * _This is a great place_
+    * _to list setup instructions_
+    * _in a simple_
+    * _easy-to-understand_
+    * _format_
+
+## Known Bugs
+
+_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+
+## Support and contact details
+
+_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+
+## Technologies Used
+
+_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+
+### License
+
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+
+Copyright (c) 2016 **_{List of contributors or company name}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,78 @@
+//- This program will recognize a vowel with a true/false statement.
+//     Input example: "a"
+//     Output Example: "true"
+//
+// - This program will recognize a vowel in a word with a true/false statement
+//     Input Example: "cat"
+//     Output Example: "true"
+//
+// - This program will add "ay" to single-letter words beginning with a vowel
+//     Input Example: "I"
+//     Output Example: "Iay"
+//
+// -  For words beginning with one or more consonants, move all of the first consecutive consonants to the end, and add "ay".
+//     Input Example: Start
+//     Output Example: artstay
+//
+// -  If the first consonants include "qu", move the "u" along with the "q". Don't forget about words like "squeal" where "qu" doesn't come first!
+//     Input Example: squirt
+//     Output Example: irtsquay
+//
+// -  For words beginning with "y", treat "y" as a consonant.
+//     Input Example: your
+//     Output Example: ouyay
 
-var vowels = ["a","e","i","o","u"];
+function determineIfVowel(word) {
+  if (word === "a" || word ===  "e"  || word ===  "i"  || word ===  "o"  || word ===  "u") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-function pigLatin(sentence) {
-  // var newSentence = []
-  var splitSentence = sentence.split(" ");
-  console.log(splitSentence);
-  for (var i = 0; i < splitSentence.length; i++) {
+function determineIfWordHasAVowel(word) {
+  var inputCharAt = word.charAt(0);
+  console.log(inputCharAt);
+  if (determineIfVowel(inputCharAt)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-   if(splitSentence[i].charAt(0) === "q") {
-     var eachChar = splitSentence[i].split("");
-     console.log(eachChar);
-     eachChar.shift();
-     console.log(eachChar);
-     eachChar.push("way");
-     splitSentence.push(eachChar.join(''));
-   }
+// function pigLatin(sentence) {
+//   // var newSentence = []
+//   var splitSentence = sentence.split(" ");
+//   console.log(splitSentence);
+//   for (var i = 0; i < splitSentence.length; i++) {
+//
+//    if(splitSentence[i].charAt(0) === "q") {
+//      var eachChar = splitSentence[i].split("");
+//      console.log(eachChar);
+//      eachChar.shift();
+//      console.log(eachChar);
+//      eachChar.push("way");
+//      splitSentence.push(eachChar.join(''));
+//    }
   //   if (sentenceArray.charAt(0)includes(vowel)) {
   //     sentenceArray.charAt(0) = "way"
   //   }
   //   return firstCharacter;
-}
-
-return splitSentence
-}
-
-
+// }
+//
+// return splitSentence
+// }
 
 $(document).ready(function() {
-var userSentence = pigLatin("The quick brown fox jumps over the lazy dog");
-console.log(userSentence);
+  $("#userInput").submit(function(event) {
+
+  console.log(determineIfVowel($("input#words").val()));
+  console.log(determineIfWordHasAVowel($("input#words").val()));
+
+  event.preventDefault();
+  });
+// var userSentence = pigLatin("The quick brown fox jumps over the lazy dog");
+// console.log(userSentence);
 });
