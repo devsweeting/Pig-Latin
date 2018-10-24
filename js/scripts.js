e28ef4ebb7606c7b12b41add47a7e690928284b3
arashi walker, Devin Sweeting and Miles Bradt <miles@>
2018-10-24 10:59:51 -0700

    run a function that will find a character and replace it at the end of an array with way

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
 
+var vowels = ["a","e","i","o","u"];
+
+function pigLatin(sentence) {
+  // var newSentence = []
+  var splitSentence = sentence.split(" ");
+  console.log(splitSentence);
+  for (var i = 0; i < splitSentence.length; i++) {
+
+   if(splitSentence[i].charAt(0) === "q") {
+     var eachChar = splitSentence[i].split("");
+     console.log(eachChar);
+     eachChar.shift();
+     console.log(eachChar);
+     eachChar.push("way");
+     splitSentence.push(eachChar.join(''));
+   }
+  //   if (sentenceArray.charAt(0)includes(vowel)) {
+  //     sentenceArray.charAt(0) = "way"
+  //   }
+  //   return firstCharacter;
+}
+
+return splitSentence
+}
+
+
+
+$(document).ready(function() {
+var userSentence = pigLatin("The quick brown fox jumps over the lazy dog");
+console.log(userSentence);
 });
